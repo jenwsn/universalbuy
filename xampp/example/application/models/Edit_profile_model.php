857094e2a359ca23674ef247cbc1fb82a52879e0
jenwsn <j.sin@uqconnect.edu.au>
2020-10-11 21:23:02 +1000

    oopsies

--- xampp/example/application/models/Edit_profile_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Forgetpsw_model extends CI_Model {
-    public function __constructor(){
-        parent::__constructor();
-    }
-    
-}
--- xampp/phpMyAdmin/vendor/williamdes/mariadb-mysql-kbs/scripts/ci/ci-mocha.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)/../../
-echo "Running in : $(pwd)"
-npm run images
-npm run report-coverage
